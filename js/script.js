92a7d37ee795e89fe67e383420298b6f0c863e9f
Денис <129628955+DenisMov@users.noreply.github.com>
2023-04-22 20:11:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+
+const counters = document.querySelectorAll('.percent__skills-numbers'),
+    lines = document.querySelectorAll('.percent__skills-line_yellow span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+ //smooth scroll and pageup 
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+
